30f6506cae44a1048519d6ed08824481bc5035b3
Alvineornela <127629007+Alvineornela@users.noreply.github.com>
2023-05-25 20:29:16 +0200

    Update lib.cpp

--- lib.cpp
+++ lib.cpp
@@ -1,1 +1,10 @@
-
+#include "lib.h"
+float ricercaMax(float a[], int n){
+    float max = a[0];
+    for(int i = 0; i<n; i++){
+        if(a[i]>max){
+            max = a[i];
+        }
+    }
+    return max;
+}
