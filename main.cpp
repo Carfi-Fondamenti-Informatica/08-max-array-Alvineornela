1bd4acd9980dcb803c16078be4e3545c2b88836b
Alvineornela <127629007+Alvineornela@users.noreply.github.com>
2023-05-25 20:34:39 +0200

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -3,15 +3,14 @@
 using namespace std;
 int main() {
     int dim;
-    cout<<"inserire da tastiera la dimensione dell'array";
     cin>>dim;
-
     float a[dim];
-    cout << "Inserire da tastiera tutti gli elementi dell'array" << endl;
     for(int j = 0; j<dim; j++){
         cin>>a[j];
     }
     cout<<ricercaMax(a,dim)<<endl;
+ 
+ 
     return 0;
 } 
   
